fda659998ee626c01c93aa71761303cc932208d6
qinlinj <justinqinlin@gmail.com>
2026-03-12 18:08:54 -0700

    feat(monotonic-stack): add solutions for LeetCode problems 1475 and 1944
    Add implementations for "Final Prices With a Special Discount in a Shop" and
    "Number of Visible People in a Queue" using monotonic stack approach.

---
+++ src/main/java/org/qinlinj/dsa/data_structure_basic/monotonic_stack/_1475_FinalPricesWithASpecialDiscountInAShop.java
@@ -0,0 +1,81 @@
+package org.qinlinj.dsa.data_structure_basic.monotonic_stack;
+import java.util.*;
+import org.qinlinj.leetcode.editor.common.*;
+// [1475] Final Prices With a Special Discount in a Shop
+public class _1475_FinalPricesWithASpecialDiscountInAShop {
+    public static void main(String[] args) {
+        Solution solution = new _1475_FinalPricesWithASpecialDiscountInAShop().new Solution();
+        // put your test code here
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        // find the first prices[j] which lower than price[i]
+        ArrayDeque<Integer> stk = new ArrayDeque<>();
+        int[] res = new int[prices.length];
+        for (int i = prices.length - 1; i >= 0; i--) {
+            while (!stk.isEmpty() && prices[i] < stk.peek()) {
+                stk.pop();
+            }
+            res[i] = stk.isEmpty() ? prices[i] : prices[i] - stk.peek();
+            stk.push(prices[i]);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+// You are given an integer array prices where prices[i] is the price of the iᵗʰ 
+// item in a shop. 
+// 
+//  There is a special discount for items in the shop. If you buy the iᵗʰ item, 
+// then you will receive a discount equivalent to prices[j] where j is the minimum 
+// index such that j > i and prices[j] <= prices[i]. Otherwise, you will not receive 
+// any discount at all. 
+// 
+//  Return an integer array answer where answer[i] is the final price you will pay 
+// for the iᵗʰ item of the shop, considering the special discount. 
+// 
+//  
+//  Example 1: 
+// 
+//  
+// Input: prices = [8,4,6,2,3]
+// Output: [4,2,4,2,3]
+// Explanation: 
+// For item 0 with price[0]=8 you will receive a discount equivalent to prices[1]=4
+// , therefore, the final price you will pay is 8 - 4 = 4.
+// For item 1 with price[1]=4 you will receive a discount equivalent to prices[3]=2
+// , therefore, the final price you will pay is 4 - 2 = 2.
+// For item 2 with price[2]=6 you will receive a discount equivalent to prices[3]=2
+// , therefore, the final price you will pay is 6 - 2 = 4.
+// For items 3 and 4 you will not receive any discount at all.
+//  
+// 
+//  Example 2: 
+// 
+//  
+// Input: prices = [1,2,3,4,5]
+// Output: [1,2,3,4,5]
+// Explanation: In this case, for all items, you will not receive any discount at 
+// all.
+//  
+// 
+//  Example 3: 
+// 
+//  
+// Input: prices = [10,1,1,6]
+// Output: [9,0,1,6]
+//  
+// 
+//  
+//  Constraints: 
+// 
+//  
+//  1 <= prices.length <= 500 
+//  1 <= prices[i] <= 1000 
+//  
+// 
+//  Related Topics Array Stack Monotonic Stack 👍 2909 👎 152
---
+++ src/main/java/org/qinlinj/dsa/data_structure_basic/monotonic_stack/_1944_NumberOfVisiblePeopleInAQueue.java
@@ -0,0 +1,78 @@
+package org.qinlinj.dsa.data_structure_basic.monotonic_stack;
+import java.util.*;
+import org.qinlinj.leetcode.editor.common.*;
+// [1944] Number of Visible People in a Queue
+public class _1944_NumberOfVisiblePeopleInAQueue {
+    public static void main(String[] args) {
+        Solution solution = new _1944_NumberOfVisiblePeopleInAQueue().new Solution();
+        // put your test code here
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] canSeePersonsCount(int[] heights) {
+        int n = heights.length;
+        int[] res = new int[n];
+        Deque<Integer> stk = new ArrayDeque<>();
+        for (int i = n - 1; i >= 0; i--) {
+            int count = 0;
+            while (!stk.isEmpty() && stk.peek() < heights[i]) {
+                stk.pop();
+                count++;
+            }
+            res[i] = stk.isEmpty() ? count : count + 1;
+            stk.push(heights[i]);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+// There are n people standing in a queue, and they numbered from 0 to n - 1 in 
+// left to right order. You are given an array heights of distinct integers where 
+// heights[i] represents the height of the iᵗʰ person. 
+// 
+//  A person can see another person to their right in the queue if everybody in 
+// between is shorter than both of them. More formally, the iᵗʰ person can see the jᵗʰ
+//  person if i < j and min(heights[i], heights[j]) > max(heights[i+1], heights[i+2
+// ], ..., heights[j-1]). 
+// 
+//  Return an array answer of length n where answer[i] is the number of people the 
+// iᵗʰ person can see to their right in the queue. 
+// 
+//  
+//  Example 1: 
+// 
+//  
+// 
+//  
+// Input: heights = [10,6,8,5,11,9]
+// Output: [3,1,2,1,1,0]
+// Explanation:
+// Person 0 can see person 1, 2, and 4.
+// Person 1 can see person 2.
+// Person 2 can see person 3 and 4.
+// Person 3 can see person 4.
+// Person 4 can see person 5.
+// Person 5 can see no one since nobody is to the right of them.
+//  
+// 
+//  Example 2: 
+// 
+//  
+// Input: heights = [5,1,2,3,10]
+// Output: [4,1,1,1,0]
+//  
+// 
+//  
+//  Constraints: 
+// 
+//  
+//  n == heights.length 
+//  1 <= n <= 10⁵ 
+//  1 <= heights[i] <= 10⁵ 
+//  All the values of heights are unique. 
+//  
+// 
+//  Related Topics Array Stack Monotonic Stack 👍 2186 👎 67
