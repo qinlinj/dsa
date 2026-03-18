41196b7f972f177e27d700aac4558598b091af20
qinlinj <justinqinlin@gmail.com>
2026-03-17 23:43:47 -0700

    feat: add solution stub for LeetCode problem 862 Shortest Subarray with Sum at Least K

---
+++ src/main/java/org/qinlinj/leetcode/editor/en/_862_ShortestSubarrayWithSumAtLeastK.java
@@ -0,0 +1,48 @@
+package org.qinlinj.leetcode.editor.en;
+import org.qinlinj.leetcode.editor.common.*;
+// [862] Shortest Subarray with Sum at Least K
+public class _862_ShortestSubarrayWithSumAtLeastK {
+    public static void main(String[] args) {
+        Solution solution = new _862_ShortestSubarrayWithSumAtLeastK().new Solution();
+        // put your test code here
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int shortestSubarray(int[] nums, int k) {
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+// Given an integer array nums and an integer k, return the length of the shortest 
+// non-empty subarray of nums with a sum of at least k. If there is no such 
+// subarray, return -1. 
+// 
+//  A subarray is a contiguous part of an array. 
+// 
+//  
+//  Example 1: 
+//  Input: nums = [1], k = 1
+// Output: 1
+//  
+//  Example 2: 
+//  Input: nums = [1,2], k = 4
+// Output: -1
+//  
+//  Example 3: 
+//  Input: nums = [2,-1,2], k = 3
+// Output: 3
+//  
+//  
+//  Constraints: 
+// 
+//  
+//  1 <= nums.length <= 10⁵ 
+//  -10⁵ <= nums[i] <= 10⁵ 
+//  1 <= k <= 10⁹ 
+//  
+// 
+//  Related Topics Array Binary Search Queue Sliding Window Heap (Priority Queue) 
+// Prefix Sum Monotonic Queue 👍 5174 👎 144
